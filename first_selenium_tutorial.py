412771424baf237a8bc888333140dbfa55788b3c
Prabha1909 <34531262+Prabha1909@users.noreply.github.com>
2018-10-19 22:16:29 +0530

    initial code for FF

--- first_selenium_tutorial.py
+++ first_selenium_tutorial.py
@@ -1,1 +1,12 @@
-import selenium
+from selenium import webdriver
+
+class FFtest():
+    def fftest_method(self):
+
+        #creating the instance of webdriver
+        driver = webdriver.Firefox(executable_path="C:\\Users\\pselvam\\Desktop\\Python_Tutorial\\Selenium\\Drivers\\geckodriver.exe")
+
+        driver.get("http://www.facebook.com")
+
+ff = FFtest
+ff.fftest_method(self="a")
